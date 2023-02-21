90f65f89573f61a519311eb59764a65cf87bb9b2
Rafael <rflernesto@gmail.com>
2023-02-21 15:44:21 -0400

    chore: declare modules

---
+++ auto-scaling-ec2/main.tf
@@ -0,0 +1,14 @@
+module "autoscaling" {
+    source = "./modules/autoscaling"
+    namespace = var.namespace
+}
+
+module "networking" {
+    source = "./module/networking"
+    namespace = var.namespace
+}
+
+module "database" {
+    source = "./module/database"
+    namespace = var.namespace
+}
