c627d250e1e5f30bd1f692523968479307da5f98
Rafael <rflernesto@gmail.com>
2023-03-26 16:13:52 -0400

    chore: add sas

--- azure/gallery-service/main.tf
+++ azure/gallery-service/main.tf
@@ -17,3 +17,34 @@ resource "azurerm_storage_container" "storage_container" {
  storage_account_name  = azurerm_storage_account.gallery_service_storage_account.name
  container_access_type = "private"
 }
+
+data "azurerm_storage_account_sas" "storage_sas" {
+  connection_string = azurerm_storage_account.gallery_service_storage_account.primary_connection_string
+ 
+  resource_types {
+    service   = false
+    container = false
+    object    = true
+  }
+ 
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+ 
+  start  = "2022-06-19T00:00:00Z"
+  expiry = "2048-06-19T00:00:00Z"
+ 
+  permissions {
+    read    = true
+    write   = false
+    delete  = false
+    list    = false
+    add     = false
+    create  = false
+    update  = false
+    process = false
+  }
+}
